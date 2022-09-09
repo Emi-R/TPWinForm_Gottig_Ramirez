a9f197598352e27e53dfc62166a89a0222038fe3
Myxolidian <88684755+Myxolidian@users.noreply.github.com>
2022-09-08 22:21:28 -0300

    Test

--- TPWinForm_Gottig_Ramirez/Program.cs
+++ TPWinForm_Gottig_Ramirez/Program.cs
@@ -14,6 +14,7 @@ namespace TPWinForm_Gottig_Ramirez
         [STAThread]
         static void Main()
         {
+            //prueba
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
